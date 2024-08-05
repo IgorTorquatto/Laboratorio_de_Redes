662dd70470e5c4859afc1a03145eabe70185befe
Igor <igor.torquato.20@gmail.com>
2024-08-05 12:56:58 -0300

    update: scan-network

--- Aula2/lab00_revisao.py
+++ Aula2/lab00_revisao.py
@@ -14,8 +14,6 @@ def scan(ip):
     answered_list = scapy.srp(arp_request_broadcast, timeout=3, verbose=1)[0] 
     client_list = []
 
-    print(answered_list)
-
     for packet in answered_list:
         client_dict = {"ip": packet[1].psrc, "mac": packet[1].hwsrc}
         client_list.append(client_dict)
@@ -26,7 +24,6 @@ def user_stop(timer):
     timer.cancel()
     print("Scan stopped.")
 
-
 subnet = "192.168.0.0/24"  
 
 timer = RepeatTimer(1.0, scan, [subnet])
@@ -34,3 +31,18 @@ timer.start()
 
 # Permitir ao usuário decidir quando parar o script
 user_stop(timer)
+
+#linha 11 : Cria um pacote ARP passando o ip da rede
+#linha 12:  Cria um pacote Ethernet que será enviado para o endereço de broadcast (todos os dispositivos na rede)
+#linha 13: Combina os pacotes ARP e Ethernet em um único pacote
+
+#linha 14: SRP -> send and receive packets . Diz que o pacote combinado será enviado, espera o tempo do timeout, em segundos, por respostas antes de considerar que não há mais respostas
+#verbose = 1 indica que será impresso informações sobre o que está acontecendo, '0' desativa essas mensagens
+#O método srp retorna uma tupla contendo duas listas: (answered_list, unanswered_list) [0] no final da chamada srp seleciona apenas a primeira lista, que é a answered_list.
+
+#linha 17 ( FOR ): Para cada resposta recebida, extrai o IP e o MAC do dispositivo e adiciona à lista de clientes.
+#Então para cada resposta é criado um dicionário com chaves ip e mac dos dispositivos da rede e cada dicionário é adicionado na lista client_list
+# cada elemento de answered_list é uma tupla de dois elementos packet[1] refere-se ao segundo elemento da tupla, que é o pacote recebido
+#psrc se refere ao endereço IP da máquina que respondeu e o hwsrc é o endereço MAC da máquina que respondeu
+
+#linha 20: É impresso a lista de clientes
